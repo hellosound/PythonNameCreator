25aef439b17d330fade7034130a9b69685134b2c
hellosound <heyhellosound@gmail.com>
2025-01-02 20:30:45 -0500

    Created beer generator
    Created beer generator

--- PythonNameGenerator/PythonNameGenerator/PythonNameGenerator.py
+++ PythonNameGenerator/PythonNameGenerator/PythonNameGenerator.py
@@ -1,1 +1,13 @@
+ascii_art = """
+.~~~~.
+i====i_
+|cccc|_)
+|cccc|
+`-==-'
+"""
 
+print("\n Bienvenido al generador de nombres de cerveza " + ascii_art)
+
+Color = input("Dime tu color favorito ")
+Nombre = input("Dime el nombre de tu ciudad ")
+print ( "Tu cerveza se llamara: " + Color + " " + Nombre )
